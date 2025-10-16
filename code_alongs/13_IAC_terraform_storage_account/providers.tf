79fb8ef8a869062f18e9c5e870a19d929cd829e5
Fredrik Kron <fkron95@gmail.com>
2025-10-16 13:35:07 +0200

    IAC terraform storage account

---
+++ code_alongs/13_IAC_terraform_storage_account/providers.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.4" # 4.4 ≤ x ≤ 5.0
+    }
+  }
+  # pessimistic operator 
+  # 1.12 ≤ x ≤ 2.0
+  required_version = "~> 1.12"
+}
+
+provider "azurerm" {
+  features {}
+
+  # Explicitly define subscription and tenant IDs
+  subscription_id = "subscription_id"
+  tenant_id       = "tenant_id"
+}
+
